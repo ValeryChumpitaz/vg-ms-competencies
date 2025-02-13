9a5b23878b3bf864ab6201e6b45a1142a325c5e3
ValeryGiselleChumpitazCaycho <111779657+ValeryChumpitaz@users.noreply.github.com>
2025-02-13 11:48:43 -0500

    Update CompetenciesController.java

--- src/main/java/pe/edu/vallegrande/vg_ms_competencies/presentation/controller/CompetenciesController.java
+++ src/main/java/pe/edu/vallegrande/vg_ms_competencies/presentation/controller/CompetenciesController.java
@@ -8,7 +8,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT})
 @RequestMapping("/common/${api.version}/competencies")
 public class CompetenciesController {
 
